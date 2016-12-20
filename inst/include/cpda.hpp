e35af83abe3401b7bb1f536489d9d93c6d92e33d
Yishin Lin <yishin.lin@utas.edu.au>
2016-12-20 18:15:31 +1100

    update inst folder

--- inst/include/cpda.hpp
+++ inst/include/cpda.hpp
@@ -1,4 +1,5 @@
 #include <cpda/pLBA.hpp>
+#include <cpda/logLik.hpp>
 #include <cpda/tnorm.hpp>
 #include <cpda/util.hpp>
 #include <cpda/dist.hpp>
--- inst/include/cpda/util.hpp
+++ inst/include/cpda/util.hpp
@@ -2,6 +2,7 @@
 arma::vec pmax(arma::vec v, double min);
 arma::vec getVec(double *x, int *nx);
 double gaussian(double y, arma::vec yhat, double h);
+double gaussian_omp(double y, arma::vec yhat, double h);
 double cquantile(arma::vec y, double q);
 double bwNRD0(arma::vec y, double m);
 std::vector<int> generateIntVec (int n);
