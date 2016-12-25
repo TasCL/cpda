45d7926c9948d3c3768eea5fbccd7823d9ca4a94
Yishin Lin <yishin.lin@utas.edu.au>
2016-12-25 18:49:09 +1100

    save backup in tests folder

--- tests/driver_logLik_plba.R
+++ tests/driver_logLik_plba.R
@@ -1,66 +1,63 @@
+###################
+## Example 1     ##
+###################
+## I demonstrate how to use the build-in logLik_plba to calculate
+## pLBA densities. set.seed(123) is to produce the same result.
 rm(list=ls())
+pVec <- c(A1=1.51, A2=1.51, b1=2.7, b2=2.7, v1=3.32, v2=2.24,
+          w1=1.51, w2=3.69, sv1=1, sv2=1, sw1=1, sw2=1, rD=0.31, 
+          swt=0.5, t0=0.08)
+
 data(lba)
-dMat <- data.matrix(d)
-head(dMat)
+d$Response <- ifelse(d$Response==0, 1, 2)
+dMat <- data.matrix(d); head(dMat)
+n <- 1e5
 
-pVec <- c(A=1.51, muv1=3.32, muw1=1.51, muv2=2.24, muw2=3.69,
-          t_delay=0.31, t_ND=0.08, b=2.7, sv=1, swt=0.5)
-tmp0 <- cpda::logLik_plba(dMat, pVec, 1e6); tmp0
+set.seed(123)
+ll0 <- cpda::logLik_plba(dMat, pVec, 1e5, h=h, m=1); ll0
 
+set.seed(123)
+llList <- cpda::logLik_plba2(dMat, pVec, 1e5, h=h, m=1); str(llList)
+# List of 4
+# $ LL      : num 327
+# $ PDF     : num [1:1000, 1:4] 1 1 1 1 1 1 1 1 1 1 ...
+# $ z       : num [1:2048, 1] 0.13 0.135 0.14 0.146 0.151 ...
+# $ PDF_hist: num [1:2048, 1] 0 0 0 0 0 ...
 
+###################
+## Example 2     ##
+###################
+## Secondly, I demonstrate use rplba2 and logLik_fft to produce identical 
+## result
 rm(list=ls())
-data(lba)
-dMat <- data.matrix(d)
-head(dMat)
+x <- cbind(rep(1:2,each=100),rep(seq(.5,2,length.out=100),2))
+pVec <- c(A1=1.51, A2=1.51, b1=2.7, b2=2.7, v1=3.32, v2=2.24,
+          w1=1.51, w2=3.69, sv1=1, sv2=1, sw1=1, sw2=1, rD=0.31, 
+          swt=0.5, t0=0.08)
+dt1 <- x[x[,1]==1,2] - pVec[15]
+dt2 <- x[x[,1]==2,2] - pVec[15]
 
-pVec <- c(b1=1, b2=1, A1=.5, A2=.5, mu1=2.4, mu2=1.6, sigma1=1, sigma2=1.2,
-          t01=.5, t02=.5)
-tmp0 <- cpda::logLik_lba(dMat, pVec, 1e8); tmp0
+set.seed(123)
+n <- 1e5
+samp <- cpda::rplba2(n, pVec); head(samp)
+dt1_ <- samp[samp[,1]==1,2] - pVec[15]
+dt2_ <- samp[samp[,1]==2,2] - pVec[15]
+logLik_fft(dt1, dt1_) + logLik_fft(dt2, dt2_)
 
+set.seed(123)
+cpda::logLik_plba(x, pVec)
 
-## Testing codes
-vec <- 1:10
-logLik_fft(1, rnorm(1e6))
-logLik_fft(1, rnorm(1e6), h=0.08)
-logLik_fft(1, rnorm(1e6), h=0.08, m=0.7)
+fft1 <- cpda::logLik_plba2(x, pVec)
+str(fft1)
+head(fft1$PDF)
+## choice RT PDF
+tmp0 <- fft1$PDF[fft1$PDF[,1] == 1, 2]
+tmp1 <- sort(x[x[,1]==1,2])
+all(tmp0==tmp1)
 
-tmp <- logLik_fft2(vec, rnorm(2e7)); log(tmp$PDF)
-log(dnorm(vec))
-
-for(i in 1:length(vec)) {
-  tmp <- logLik_fft(vec[i], rnorm(1e7), h=0.008); 
-  cat(tmp,"\n")
-}
-
-tmp <- logLik_fft2(vec, rnorm(1e7), h=0.008); 
-log(tmp$PDF)
-
-
-
-x  <- rnorm(1e3)
-samp <- rnorm(1e6)
-h <- 0.8*bw.nrd0(samp)
-log(logLik_pw(1, samp, h))
-logLik_fft(1, samp, h)
+tmp3 <- fft1$PDF[fft1$PDF[,1] == 2,2]
+tmp4 <- sort(x[x[,1]==1,2])
+all(tmp3==tmp4)
 
 
-## add bandwidth and multiplier 
-
-fft <- exp(logLik_fft2(x, samp)[["PDF"]])
-pw <- exp(logLik_pw())
-plot(x, fft)
-
-log(dnorm(1))
-
-
-pVec <- c(mu=5, sigma=1)
-y    <- sort(rnorm(1e3, pVec[1], pVec[2]))
-
-ll1 <- logLik_norm(y, pVec, 1e6)
-ll2 <- logLik_norm2(y, pVec, 1e6)
-str(ll1); str(ll2)
-
-plot(ll2$z, ll2$PDF_hist, type="l", lty=2,
-     main="Normal Distribution",xlab="x",ylab="L(x|beta)")
-lines(y, ll2$PDF, col="red", lwd = 1.5)
 
--- tests/driver_rplba.R
+++ tests/driver_rplba.R
@@ -1,34 +1,113 @@
-rm(list=ls())
-pVec <- c(A=1.51, b=2.7, muv1=3.32, muv2=2.24, t_ND=0.08, muw1=1.51, 
-          muw2=3.69, t_delay=0.31, sv=1, swt=0.5)
+library(cpda)
+n <- 1e5
+pVec1 <- c(A=1.51, b=2.7, v1=3.32, v2=2.24,  w1=1.51,  w2=3.69,  
+           sv=1, rD=0.31, swt=0.5, t0=0.08)
 
-data(lba)
-dMat <- data.matrix(d); head(dMat)
+pVec2 <- c(A1=1.51, A2=1.51, b1=2.7, b2=2.7, v1=3.32, v2=2.24,
+           w1=1.51, w2=3.69, sv1=1, sv2=1, sw1=1, sw2=1, rD=0.31, 
+           swt=0.5, t0=0.08)
+
+pVec3.1 <- c(A1=1.51, A2=1.51, B1=1.2, B2=1.2, C1=.3, C2=.3, v1=3.32, v2=2.24,
+           w1=1.51, w2=3.69, sv1=1, sv2=1, sw1=1, sw2=1, rD=0.1, tD=.1,
+           swt=0.5, t0=0.08)
+pVec3.2 <- c(A1=1.51, A2=1.51, B1=1.2, B2=1.2, C1=.3, C2=.3, v1=3.32, v2=2.24,
+           w1=1.51, w2=3.69, sv1=1, sv2=1, sw1=1, sw2=1, rD=0.1, tD=.15,
+           swt=0.5, t0=0.08)
+pVec3.3 <- c(A1=1.51, A2=1.51, B1=1.2, B2=1.2, C1=.3, C2=.3, v1=3.32, v2=2.24,
+           w1=1.51, w2=3.69, sv1=1, sv2=1, sw1=1, sw2=1, rD=0.15, tD=.1,
+           swt=0.5, t0=0.08)
+
 
 set.seed(123)
-samp   <- cpda::rplba(1e5, pVec); head(samp)
-h   <- 0.8*bw.nrd0(samp[,2]); h
-set.seed(123)
-ll0 <- cpda::logLik_plba(dMat, pVec, 1e5, h=h, m=1); ll0
+system.time(dat1 <- cpda::rplba1(n, pVec1))
 
 set.seed(123)
-llList <- cpda::logLik_plba2(dMat, pVec, 1e5, h=h, m=1); 
-str(llList)
+system.time(dat2 <- cpda::rplba2(n, pVec2))
 
-## List of 4
-## $ LL      : num -7277
-## $ PDF     : num [1:1000, 1] -23 -23 -23 -23 -23 ...
-## $ z       : num [1:2048, 1] 0.175 0.179 0.184 0.188 0.192 ...
-## $ PDF_hist: num [1:2048, 1] 0 0 0 0 0 0 0 0 0 0 ...
-
-## Here I demonstrate use rplba and logLik_fft to produce identical 
-## result
 set.seed(123)
-DTMat  <- cpda::choiceDT(dMat, pVec)
-time1  <- sort(DTMat[DTMat[,1] == 1, 2])
-time2  <- sort(DTMat[DTMat[,1] == 2, 2])
-time1_ <- sort(samp[samp[,1] == 1, 2])
-time2_ <- sort(samp[samp[,1] == 2, 2])
-ll1 <- cpda::logLik_fft(time1, time1_, h=h, m=1); ll1
-ll2 <- cpda::logLik_fft(time2, time2_, h=h, m=1); ll2
-print(ll1+ll2)
+system.time(dat3 <- cpda::rplbaR1(n, pVec1))
+
+set.seed(123)
+system.time(dat4 <- cpda::rplbaR2(n, pVec2))
+
+set.seed(123)
+system.time(dat5 <- cpda::rplbaR3(n, pVec3))
+head(dat5)
+
+all(dat1[,1] == dat2[,1])
+all(dat1[,2] == dat2[,2])
+
+all(dat1[,1] == dat3[,1])
+all(dat1[,2] == dat3[,2])
+
+all(dat2[,1] == dat4[,1])
+all(dat2[,2] == dat4[,2])
+
+tmp1 <- data.frame(choice=factor(dat1[,1]), rt=dat1[,2])
+tmp2 <- data.frame(choice=factor(dat2[,1]), rt=dat2[,2])
+tmp3 <- data.frame(choice=factor(dat3[,1]), rt=dat3[,2])
+tmp4 <- data.frame(choice=factor(dat4[,1]), rt=dat4[,2])
+tmp1$fun <- "rplba1"
+tmp2$fun <- "rplba2"
+tmp3$fun <- "rplba1-R"
+tmp4$fun <- "rplba2-R"
+tmp0 <- rbind(tmp1, tmp2, tmp3, tmp4)
+tmp0$fun <- factor(tmp0$fun)
+
+
+n <- 1e5
+set.seed(123); system.time(dat5.1 <- cpda::rplbaR3(n, pVec3.1))
+set.seed(123); system.time(dat5.2 <- cpda::rplbaR3(n, pVec3.2))
+set.seed(123); system.time(dat5.3 <- cpda::rplbaR3(n, pVec3.3))
+set.seed(123); system.time(dat6.1 <- cpda::rplba3(n, pVec3.1))
+set.seed(123); system.time(dat6.2 <- cpda::rplba3(n, pVec3.2))
+set.seed(123); system.time(dat6.3 <- cpda::rplba3(n, pVec3.3))
+
+tmp5.1 <- data.frame(choice=factor(dat5.1[,1]), rt=dat5.1[,2])
+tmp5.2 <- data.frame(choice=factor(dat5.2[,1]), rt=dat5.2[,2])
+tmp5.3 <- data.frame(choice=factor(dat5.3[,1]), rt=dat5.3[,2])
+tmp6.1 <- data.frame(choice=factor(dat6.1[,1]), rt=dat6.1[,2])
+tmp6.2 <- data.frame(choice=factor(dat6.2[,1]), rt=dat6.2[,2])
+tmp6.3 <- data.frame(choice=factor(dat6.3[,1]), rt=dat6.3[,2])
+
+
+tmp5.1$fun <- "R"
+tmp5.2$fun <- "R"
+tmp5.3$fun <- "R"
+tmp6.1$fun <- "C"
+tmp6.2$fun <- "C"
+tmp6.3$fun <- "C"
+
+tmp5.1$vec <- "1"
+tmp5.2$vec <- "2"
+tmp5.3$vec <- "3"
+tmp6.1$vec <- "1"
+tmp6.2$vec <- "2"
+tmp6.3$vec <- "3"
+
+tmp0 <- rbind(tmp5.1, tmp5.2, tmp5.3, tmp6.1, tmp6.2, tmp6.3)
+tmp0$fun <- factor(tmp0$fun)
+
+
+
+library(gridExtra)
+library(lattice)
+histogram( ~rt | fun + choice , data = tmp0, breaks="fd", type="density", 
+                 xlab="Response Time (s)", 
+                 panel=function(x, ...) {
+                     panel.histogram(x, ...)
+                     panel.densityplot(x, darg=list(kernel="gaussian"),...)
+                   })
+
+
+cb <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", 
+        "#0072B2", "#D55E00", "#CC79A7")
+library(ggplot2)
+ggplot(data = tmp0, aes(x = rt, fill=fun, color=fun)) +
+  geom_density(alpha=0.2) +
+  facet_grid(vec~ choice) +
+  scale_fill_manual(values=cb)
+
+# hist(tmp1$rt, breaks="fd", freq=FALSE)
+# lines(density(tmp1$rt), col="red", lwd=2)
+
