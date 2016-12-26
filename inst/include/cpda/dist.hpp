8c6795fbc86e72e661627f9a336a5e9d61176332
Yishin Lin <yishin.lin@utas.edu.au>
2016-12-26 12:37:51 +1100

    update inst folder

--- inst/include/cpda/dist.hpp
+++ inst/include/cpda/dist.hpp
@@ -1,5 +1,4 @@
 #include <RcppArmadillo.h>
-arma::mat choiceDT(arma::mat data, arma::vec pVec);
 double logLik_norm(arma::vec object, arma::vec pVec, int ns);
 Rcpp::List logLik_norm2(arma::vec object, arma::vec pVec, int ns);
 double logLik_plba(arma::mat object, arma::vec pVec, int ns);
--- inst/include/cpda/pLBA.hpp
+++
@@ -1,9 +0,0 @@
-#include <RcppArmadillo.h>
-
-arma::vec getEdges(arma::vec z) ;
-arma::vec getFilter(double m, double M, double h) ;
-arma::vec density(arma::vec y, arma::vec be, double dt) ;
-arma::mat rlba(int n, arma::vec pVec);
-arma::mat rplba(int n, arma::vec pVec);
-arma::mat choiceDT(arma::mat data, arma::vec pVec);
-
